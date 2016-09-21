b1f5d8fa25897207a0ae12992509f8a3510c1f6f
Natu Myers <xoria7@gmail.com>
2016-09-21 14:22:03 -0400

    update

--- 3dTest1/ViewController.swift
+++ 3dTest1/ViewController.swift
@@ -18,6 +18,27 @@ class ViewController: UIViewController {
         
         let sceneView = SCNView(frame: self.view.frame)
         self.view.addSubview(sceneView)
+
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3(x: 0.0, y: 0.0, z: 3.0)
+        
+        let light = SCNLight()
+        light.type = SCNLightTypeOmni
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3(x: 1.5, y: 1.5, z: 1.5)
+        
+        let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        
+        scene.rootNode.addChildNode(lightNode)
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(cubeNode)
     }
 
     override func didReceiveMemoryWarning() {
